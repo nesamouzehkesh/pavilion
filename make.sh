53e4cd2770c64ceec326ff48a534328a06ccf51d
nesamouzehkesh <n.mouzehkesh@gmail.com>
2015-08-25 11:24:06 +1000

    master: update readme and add make file

--- make.sh
+++ make.sh
@@ -1,11 +1,11 @@
 sudo composer install
-mkdir ./app/cache/
-chmod -R 777 ./app/config/parameters.yml
-chmod -R 777 ./app/cache/
-chmod -R 777 ./app/logs/
-chmod -R 777 ./app/media/
-php app/console doctrine:schema:update --force
-php app/console doctrine:fixtures:load
-php app/console cache:clear --env=prod --no-debug
-php app/console assets:install --symlink --relative
-php app/console assetic:dump --watch
+sudo mkdir ./app/media/
+sudo chmod -R 777 ./app/config/parameters.yml
+sudo chmod -R 777 ./app/cache/
+sudo chmod -R 777 ./app/logs/
+sudo chmod -R 777 ./app/media/
+sudo php app/console doctrine:schema:update --force
+sudo php app/console doctrine:fixtures:load
+sudo php app/console cache:clear --env=prod --no-debug
+sudo php app/console assets:install --symlink --relative
+sudo php app/console assetic:dump --watch
