aa7053769ab7028b26b88a9e2e933845a1e4dac0
nesamouzehkesh <n.mouzehkesh@gmail.com>
2015-08-25 11:18:44 +1000

    master: update readme and add make file

--- make.sh
+++ make.sh
@@ -6,6 +6,6 @@ chmod -R 777 ./app/logs/
 chmod -R 777 ./app/media/
 php app/console doctrine:schema:update --force
 php app/console doctrine:fixtures:load
-$ php app/console cache:clear --env=prod --no-debug
+php app/console cache:clear --env=prod --no-debug
 php app/console assets:install --symlink --relative
 php app/console assetic:dump --watch
