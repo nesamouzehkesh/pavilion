c193cc50f21d12275a7322ee87682b7047e5499e
nesamouzehkesh <n.mouzehkesh@gmail.com>
2015-09-28 09:41:39 +1000

    develop: add data

--- src/AppBundle/DataFixtures/ORM/LoadSampleData.php
+++ src/AppBundle/DataFixtures/ORM/LoadSampleData.php
@@ -21,6 +21,7 @@ use ProductBundle\Entity\Product;
  */
 class LoadSampleData implements FixtureInterface
 {
+    
     private $data = array(
         'pages' => 0,
         'pagesData' => array(
@@ -28,19 +29,52 @@ class LoadSampleData implements FixtureInterface
                 'title'   => 'Order',
                 'url'     => '/order',
                 'content' => 'We handmade your carpet based on your design, whether its an abstract painting, 
-                        a family picture a company logo or a pattern of your choice from our gallery. 
-                        First you select your design, the size, colour and specific manner in which 
-                        your unique, high quality Persian carpet will be made. Our intelligent 
-                        software turns your design into a recipe for a handmade carpet using the 
-                        best matching colours and patterns and you made your order.                         
-                        Then we choose the best organic materials which are hand-carded and hand-dyed, 
-                        producing a rich, harmonious effect for your carpet. Your carpet will be 
-                        woven by skilful hands of our craftsmen. The result is an eye-catching masterpiece 
-                        which is unique in both its built and design.
-                        We bear with you throughout the entire design and production process so that you 
-                        will be able to make trivial or substantive alteration as your carpet is being 
-                        made in our workshop. When your Carpet is ready we will ship it free of any charge
-                        to your address.
+                        a family picture a company logo or a pattern of your choice from our gallery.<br><br>
+                            <center><a href="http://54.153.192.23/shopping/order/create" class="btn btn-default btn-xxlg">Make your Order</a></center>
+                            <p class="margin-top-20">
+                                First you select your design, the size, colour and specific manner in which 
+                                your unique, high quality Persian carpet will be made. Our intelligent 
+                                software turns your design into a recipe for a handmade carpet using the 
+                                best matching colours and patterns and you made your order.                         
+                                Then we choose the best organic materials which are hand-carded and hand-dyed, 
+                                producing a rich, harmonious effect for your carpet. Your carpet will be 
+                                woven by skilful hands of our craftsmen.
+                            </p>
+                            <div class="row no-padding">
+                                <div class="col-md-3 col-sm-3 col-xs-6">
+                                    <center>
+                                        <img src="theme/img/step1.png"  alt="Google Map" /><br>
+                                        Upload Photo
+                                    </center>
+                                </div>    
+                                <div class="col-md-3 col-sm-3 col-xs-6">
+                                    <center>
+                                        <img src="theme/img/step2.png"  alt="Google Map" /><br>
+                                        Design Carpet
+                                    </center>
+                                </div>    
+                                <div class="col-md-3 col-sm-3 col-xs-6">
+                                    <center>
+                                        <img src="theme/img/step3.png"  alt="Google Map" /><br>
+                                        Payment
+                                    </center>
+                                </div>    
+                                <div class="col-md-3 col-sm-3 col-xs-6">
+                                    <center>
+                                        <img src="theme/img/step4.png"  alt="Google Map" /><br>
+                                        Delivery
+                                    </center>
+                                </div>    
+                            </div> 
+
+                        <p class="margin-top-20">
+                            The result is an eye-catching masterpiece 
+                            which is unique in both its built and design.
+                            We bear with you throughout the entire design and production process so that you 
+                            will be able to make trivial or substantive alteration as your carpet is being 
+                            made in our workshop. When your Carpet is ready we will ship it free of any charge
+                            to your address.
+                        </p>
                 '
             ),
             array(
@@ -52,29 +86,76 @@ class LoadSampleData implements FixtureInterface
                     become a painting frame on your wall, a family picture in your room, a wedding 
                     present to your friend, or your company logo to carry your passion for quality to 
                     your business partners. If you still prefer traditional Iranian designs, you can
-                    browse our extensive collection of royalty-free designs, ready to be ordered.'
-            ),
-            array(
-                'title'   => 'Aboutus',
+                    browse our extensive collection of royalty-free designs, ready to be ordered.<br>
+                    <div class="row hidden-xs">
+                        <div class="col-md-3 col-sm-3">
+                            <center><a href="" title=""><img src="theme/img/c1.png" alt="..."><br>Gallery 1</a></center>
+                        </div>
+                        <div class="col-md-3 col-sm-3">
+                            <center><a href="" title=""><img src="theme/img/c3.png" alt="..."><br>Gallery 2</a></center>
+                        </div>
+                        <div class="col-md-3 col-sm-3">
+                            <center><a href="" title=""><img src="theme/img/c5.png" alt="..."><br>Gallery 3</a></center>
+                        </div>
+                        <div class="col-md-3 col-sm-3">
+                            <center><a href="" title=""><img src="theme/img/c4.png" alt="..."><br>Gallery 4</a></center>
+                        </div>
+                    </div>
+                    <div class="row visible-xs">
+                        <div class="col-xs-6">
+                            <center><a href="" title=""><img src="theme/img/c1-xs.png" alt="..."><br>Gallery 1</a></center>
+                        </div>
+                        <div class="col-xs-6">
+                            <center><a href="" title=""><img src="theme/img/c3-xs.png" alt="..."><br>Gallery 2</a></center>
+                        </div>
+                    </div>
+                    <div class="row visible-xs margin-top-10">
+                        <div class="col-xs-6">
+                            <center><a href="" title=""><img src="theme/img/c5-xs.png" alt="..."><br>Gallery 3</a></center>
+                        </div>
+                        <div class="col-xs-6">
+                            <center><a href="" title=""><img src="theme/img/c4-xs.png" alt="..."><br>Gallery 4</a></center>
+                        </div>
+                    </div>                      
+                    '
+                ),
+                array(
+                    'title'   => 'Aboutus',
                 'url'     => '/aboutus',
                 'content' => 'We are a family of designers and craftsmen working together to revive 
                     the most iconic product of the Iranian art and culture, the Persian carpet. You 
                     can mix the authenticity of the handmade Persian carpets with any design of your 
                     own. Your carpet will be woven by skilful hands of our craftsmen, with knots of 
                     silk and wool on strings of love and passion. The product is a masterpiece with 
-                    quality of ancient persian carpets and the unique design of your own taste.'
+                    quality of ancient persian carpets and the unique design of your own taste.<br>
+                    <center><img src="theme/img/zibaf.png" alt="Zibaf"></center>
+                    '
             ),
             array(
                 'title'   => 'Contact',
                 'url'     => '/contact',
                 'content' => 'We are located in Tabriz, IRAN, one of the world’s greatest hubs for Persian handmade carpet and rug industry. 
+                    <center>
+                    <ul class="list-inline banner-social-buttons">
+                        <li>
+                            <a href="https://twitter.com/zibaf" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                        </li>
+                        <li>
+                            <a href="https://facebook.com/zibaf" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                        </li>
+                        <li>
+                            <a href="https://plus.google.com/zibaf" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+                        </li>
+                    </ul>
+                    </center>
                     <address>
                         <strong>Address</strong><br>
                             Unit 2, Tech Hub Tabriz Islamic Art University<br>
                             Azadi Street, Tabriz - Iran<br>
                             Postcode: 5164736931<br>
                         <a href="mailto:info@zibaf.com">info@zibaf.com</a>
-                    </address>'
+                    </address>
+                    '
             ),
         ),
         'products' => 10,
