0371d98a1ddc2375410551e4562e68d43f9624c4
nesamouzehkesh <n.mouzehkesh@gmail.com>
2015-11-12 12:49:34 +1100

    feature/5: reorganaizing

--- warmup.sh
+++ warmup.sh
@@ -1,3 +1,6 @@
+sudo chmod -R 777 ./app/cache
+sudo rm -R ./app/cache
+sudo mkdir ./app/cache
 sudo php app/console cache:clear --env=prod
 sudo php app/console assetic:dump
 sudo chmod -R 777 ./app/cache
